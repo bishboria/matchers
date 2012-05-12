3c8b16908cee5f9748c40a1a803fb568762dcd88
Stuart Gale <bishboria@gmail.com>
2012-05-12 22:36:41 +0100

    Showing correct error for equals method...
    Time to separate before adding more messages.

--- examples/example_matchers.rb
+++ examples/example_matchers.rb
@@ -12,7 +12,7 @@ check_that [1, 2], does_not( have_length( greater_than 2 )) # true
 
 # Introducing errors
 
-puts check_that( check_that(1, equal_to(2)),
+check_that( check_that(1, equal_to(2)),
             equal_to("1 is not equal to 2"))
 
 puts check_that 1, equals(2)
