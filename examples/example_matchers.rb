472d0de1fe9e3fedfbbde02e4f0fabea014f059a
Stuart Gale <bishboria@gmail.com>
2012-05-13 01:11:02 +0100

    .

--- examples/example_matchers.rb
+++ examples/example_matchers.rb
@@ -14,29 +14,31 @@ end
 
 # Introducing errors
 
-check_that 1, equals(2)
-puts check_that( check_that(1, is_not( equal_to 1 )),
-                 equals( "error: expected 1 to not equal 1" ))
+examples do
+  check_that 1, equals(2)
+  puts check_that( check_that(1, is_not( equal_to 1 )),
+                  equals( "error: expected 1 to not equal 1" ))
 
-puts check_that( check_that(1, equals(2)),
-                 equals("error: expected 1 to equal 2"))
+  puts check_that( check_that(1, equals(2)),
+                  equals("error: expected 1 to equal 2"))
 
-puts check_that( check_that(1, greater_than(2)),
-                 equals("error: expected 1 to be greater than 2"))
+  puts check_that( check_that(1, greater_than(2)),
+                  equals("error: expected 1 to be greater than 2"))
 
-puts check_that( check_that(2, less_than(1)),
-                 equals("error: expected 2 to be less than 1"))
+  puts check_that( check_that(2, less_than(1)),
+                  equals("error: expected 2 to be less than 1"))
 
-puts check_that( check_that([1,2], has_length(3)),
-                 equals("error: expected [1, 2] to have length 3"))
+  puts check_that( check_that([1,2], has_length(3)),
+                  equals("error: expected [1, 2] to have length 3"))
 
-puts check_that( check_that([1,2], has_count(3)),
-                 equals("error: expected [1, 2] to have count 3"))
+  puts check_that( check_that([1,2], has_count(3)),
+                  equals("error: expected [1, 2] to have count 3"))
 
-puts check_that( check_that([1,2], has_length( less_than 2 )),
-                 equals( "error: expected [1, 2] to have length be less than 2" ))
+  puts check_that( check_that([1,2], has_length( less_than 2 )),
+                  equals( "error: expected [1, 2] to have length be less than 2" ))
 
-puts check_that( check_that([1,2], has_count( less_than 2 )),
-                 equals( "error: expected [1, 2] to have count be less than 2" ))
+  puts check_that( check_that([1,2], has_count( less_than 2 )),
+                  equals( "error: expected [1, 2] to have count be less than 2" ))
 
-puts check_that("hello world", contains( "hello" ))
+  puts check_that("hello world", contains( "hello" ))
+end
