3a4ff5f5ecbe6d30a2c2185ffc2cf99ca8927795
Stuart Gale <bishboria@gmail.com>
2012-05-13 03:15:13 +0100

    .

--- lib/matchers.rb
+++ lib/matchers.rb
@@ -52,6 +52,22 @@ module RecordErrors
   end
 end
 
+class Reporter
+  def initialize all_errors, total_examples, start, stop
+    @all_errors = all_errors
+    @total_examples = total_examples
+    @start = start
+    @stop = stop
+  end
+
+  def report
+"""
+#{@all_errors.join "\n"}
+#{@total_examples} examples run in #{@stop - @start} seconds
+"""
+  end
+end
+
 module Examples
   class Matcher
     include RecordErrors
