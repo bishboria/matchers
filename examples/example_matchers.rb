6244158a7d2eb044cf2efd4408509245f3b1c90e
Stuart Gale <bishboria@gmail.com>
2012-05-13 05:02:04 +0100

    This example is a good reason for fixing recorder
    It shows the error collecting is wrong as it wipes the error before it can be compared. This is only really shown as I use check_that to check check_that.
    
    Should collect all messages and result, then filter the errors at end.

--- examples/example_matchers.rb
+++ examples/example_matchers.rb
@@ -37,6 +37,6 @@ examples do
   check_that( check_that([1,2], has_length( less_than 2 )),
               equals( "error: expected [1, 2] to have length be less than 2" ))
 
-  check_that( check_that([1,2], has_count( less_than 2 )),
+  check_that( check_that([1,2], has_count( less_than 3 )),
               equals( "error: expected [1, 2] to have count be less than 2" ))
 end
